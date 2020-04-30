06b33767aad6a1a51f7c9890c938902a61633ecf
Toby <toby.james.thomas@gmail.com>
2020-04-30 14:52:36 -0400

    Add linked list

--- linked-list/linked-list.lua
+++ linked-list/linked-list.lua
@@ -1,35 +1,89 @@
+Node = {}
+
+function Node:Create(val)
+  local v = {}
+  v.value = val
+  v.next = nil
+  v.prev = nil
+  return v
+end
+
 return function()
   local list = {}
-  list.values = {}
+
+  local head = nil
+  local tail = nil
+  local count = 0
 
   function list:push(val)
-    table.insert(self.values, val)
+    node = Node:Create(val)
+
+    if tail == nil then
+      head = node
+    else
+      node.prev = tail
+      tail.next = node
+    end
+
+    tail = node
+    count = count + 1
   end
 
   function list:pop()
-    return table.remove(self.values)
+    local val = nil
+
+    if tail ~= nil then
+      val = tail.value
+      tail = tail.prev
+      count = count - 1
+    end
+
+    return val
   end
 
   function list:unshift(val)
-    table.insert(self.values, 1, val)
+    node = Node:Create(val)
+
+    if head == nil then
+      tail = node
+    else
+      node.next = head
+      head.prev = node
+    end
+
+    head = node
+    count = count + 1
   end
 
   function list:shift()
-    return table.remove(self.values, 1)
+    local val = nil
+
+    if head ~= nil then
+      val = head.value
+      head = head.next
+      count = count - 1
+    end
+
+    return val
   end
 
   function list:count()
-    return #self.values
+    return count
   end
 
   function list:delete(val)
-    local i = 1
-    while i <= self:count() do
-      if self.values[i] == val then
-        table.remove(self.values, i)
-      else
-        i = i + 1
+    local cur = head
+
+    while cur ~= nil do
+      if cur.value == val then
+        if cur == head then head = cur.next end
+        if cur == tail then tail = cur.prev end
+        if cur.prev ~= nil then cur.prev.next = cur.next end
+        if cur.next ~= nil then cur.next.prev = cur.prev end
+        count = count - 1
       end
+
+      cur = cur.next
     end
   end
 
